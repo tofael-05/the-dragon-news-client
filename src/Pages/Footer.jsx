b4921995bc7d27dd52d5e1c6500cf0a089ce9315
tofae <arnobtuhin,j@gmail.com>
2023-05-06 20:58:48 +0600

    addedssss

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h5 className="text-center ">Footer</h5>
+            <h5 className="text-center ">Foote</h5>
         </div>
     );
 };
